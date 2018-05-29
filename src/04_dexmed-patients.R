37e4a2b7eec8e3bb05adefcd0b9dc4af0c0ef72f
bgulbis <brian.gulbis@gmail.com>
2018-05-29 12:33:43 -0500

    limit to pts getting both dexmed and clon

--- src/04_dexmed-patients.R
+++ src/04_dexmed-patients.R
@@ -4,6 +4,7 @@ library(edwr)
 
 dir_raw <- "data/raw/dexmed"
 tz <- "US/Central"
+picu <- "HC PICU"
 
 dirr::gzip_files(dir_raw)
 
@@ -22,3 +23,22 @@ mbo_id <- concat_encounters(raw_pts$millennium.id, 1000)
 # run MBO queries
 #   * Medications - Inpatient - All
 
+raw_meds <- read_data(dir_raw, "meds-inpt", FALSE) %>%
+    as.meds_inpt()
+
+meds_dexm <- raw_meds %>%
+    filter(
+        med == "dexmedetomidine",
+        !is.null(event.tag),
+        med.location == picu
+    ) 
+
+meds_clon <- raw_meds %>%
+    filter(
+        med == "clonidine",
+        med.location == picu
+    ) 
+
+pts_include <- meds_dexm %>%
+    semi_join(meds_clon, by = "millennium.id") %>%
+    distinct(millennium.id)
