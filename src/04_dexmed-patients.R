8dd69db5d61ca064a662438e0aa7026800861939
bgulbis <brian.gulbis@gmail.com>
2018-06-07 13:47:13 -0500

    summarize daily dexmed dosing, other meds

--- src/04_dexmed-patients.R
+++ src/04_dexmed-patients.R
@@ -79,10 +79,16 @@ meds <- read_data(dir_raw, "meds-all-inpt", FALSE) %>%
     as.meds_inpt() %>%
     semi_join(dc, by = "millennium.id")
 
-vent <- read_data(dir_raw, "vent-times", FALSE) %>%
+data_vent <- read_data(dir_raw, "vent-times", FALSE) %>%
     as.vent_times() %>%
     tidy_data(dc) %>%
     filter(vent.duration > 0) %>%
+    semi_join(dc, by = "millennium.id")
+
+weights <- read_data(dir_raw, "events-measures", FALSE) %>%
+    as.events(order_var = FALSE) %>%
+    filter(event == "weight") %>%
+    mutate_at("event.result", as.numeric) %>%
     semi_join(dc, by = "millennium.id")
 
 # dexmedetomidine --------------------------------------
@@ -91,6 +97,14 @@ dexmed <- meds_dexm %>%
     semi_join(dc, by = "millennium.id") %>%
     calc_runtime() %>%
     summarize_data()
+
+dexmed_daily <- meds_dexm %>%
+    semi_join(dc, by = "millennium.id") %>%
+    arrange(millennium.id, med.datetime) %>%
+    mutate(med.day = floor_date(med.datetime, unit = "days")) %>%
+    calc_runtime(med.day) %>%
+    summarize_data(med.day) %>%
+    filter(cum.dose > 0)
 
 clon <- meds_clon %>%
     semi_join(dc, by = "millennium.id") %>%
@@ -126,3 +140,49 @@ dexmed_clon <- dexmed %>%
     )
 
 # get order.id for lorazepam, methadone - only want scheduled meds
+
+meds_alt <- meds %>%
+    filter(
+        med %in% c("lorazepam", "methadone"),
+        med.location == picu
+    ) %>%
+    mutate(orig.order.id = order.parent.id) %>%
+    mutate_at("orig.order.id", na_if, y = 0L) %>%
+    mutate_at("orig.order.id", funs(coalesce(., order.id)))
+
+mbo_order <- concat_encounters(meds_alt$orig.order.id)
+
+# run MBO query
+#   * Orders Meds - Details - by Order Id
+
+orders <- read_data(dir_raw, "orders-details", FALSE) %>%
+    as.order_detail(extras = list("order.product" = "Mnemonic (Product)"))
+
+data_meds_other <- meds_alt %>%
+    left_join(
+        orders[c("millennium.id", "order.id", "prn")], 
+        by = c("millennium.id", "orig.order.id" = "order.id")
+    ) %>%
+    filter(prn == "Scheduled") %>%
+    left_join(
+        dexmed[c("millennium.id", "start.datetime")],
+        by = "millennium.id"
+    ) %>%
+    filter(med.datetime > start.datetime) %>%
+    arrange(millennium.id, med.datetime) %>%
+    distinct(millennium.id, med, .keep_all = TRUE) %>%
+    mutate(
+        time.dexmed = difftime(
+            med.datetime,
+            start.datetime,
+            units = "days"
+        )
+    ) %>%
+    select(
+        millennium.id,
+        med,
+        med.datetime,
+        med.dose,
+        med.dose.units,
+        time.dexmed
+    )
