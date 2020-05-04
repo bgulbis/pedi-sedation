aad8b21969ab3fa171fbc896864e45579eddaeba
Brian Gulbis <brian.gulbis@gmail.com>
2020-05-04 16:41:19 -0500

    Update dexmed_minus_clon.sql

--- qry/dexmed_minus_clon.sql
+++ qry/dexmed_minus_clon.sql
@@ -27,74 +27,21 @@ WHERE
 		)
 		AND ENCNTR_LOC_HIST.LOC_NURSE_UNIT_CD = 1101851
 	)
-	AND (
-		CLINICAL_EVENT.EVENT_END_DT_TM + 0
-			BETWEEN DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', pi_to_gmt(TRUNC(SYSDATE), pi_time_zone(2, @Variable('BOUSER'))),
-				'Yesterday', pi_to_gmt(TRUNC(SYSDATE) - 1, pi_time_zone(2, @Variable('BOUSER'))),
-				'Week to Date', pi_to_gmt(TRUNC(SYSDATE, 'DAY'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Week', pi_to_gmt(TRUNC(SYSDATE - 7, 'DAY'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Month', pi_to_gmt(TRUNC(ADD_MONTHS(SYSDATE, -1), 'MONTH'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Month to Date', pi_to_gmt(TRUNC(SYSDATE - 1, 'MONTH'), pi_time_zone(2, @Variable('BOUSER'))),
-				'User-defined', pi_to_gmt(
-					TO_DATE(
-						@Prompt('Enter begin date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:80),
-						pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
-					),
-					pi_time_zone(1, @Variable('BOUSER'))),
-				'N Days Prior', pi_to_gmt(SYSDATE - @Prompt('Days Prior to Now', 'N', , mono, free, persistent, {'0'}, User:2080), pi_time_zone(2, @Variable('BOUSER')))
-			)
-			AND DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', pi_to_gmt(TRUNC(SYSDATE) + (86399 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Yesterday', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Week to Date', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Week', pi_to_gmt(TRUNC(SYSDATE, 'DAY') - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Month', pi_to_gmt(TRUNC(SYSDATE, 'MONTH') - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Month to Date', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'User-defined', pi_to_gmt(
-					TO_DATE(
-						@Prompt('Enter end date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 23:59:59'}, User:81),
-						pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
-					),
-					pi_time_zone(1, @Variable('BOUSER'))),
-				'N Days Prior', pi_to_gmt(SYSDATE, pi_time_zone(2, @Variable('BOUSER')))
-			)
-		AND CLINICAL_EVENT.EVENT_END_DT_TM
-			BETWEEN DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', TRUNC(SYSDATE),
-				'Yesterday', TRUNC(SYSDATE) - 1,
-				'Week to Date', TRUNC(SYSDATE, 'DAY'),
-				'Last Week', TRUNC(SYSDATE - 7, 'DAY'),
-				'Last Month', TRUNC(ADD_MONTHS(SYSDATE, -1), 'MONTH'),
-				'Month to Date', TRUNC(SYSDATE - 1, 'MONTH'),
-				'User-defined', DECODE(
-					@Prompt('Enter begin date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:80),
-					'01/01/1800 00:00:00',
-					'',
-					@Variable('Enter begin date (Leave as 01/01/1800 if using a Relative Date)')
-				),
-				'N Days Prior', SYSDATE - @Prompt('Days Prior to Now', 'N', , mono, free, persistent, {0}, User:2080)
-			) - 1
-			AND DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', TRUNC(SYSDATE) + (86399 / 86400),
-				'Yesterday', TRUNC(SYSDATE) - (1 / 86400),
-				'Week to Date', TRUNC(SYSDATE) - (1 / 86400),
-				'Last Week', TRUNC(SYSDATE, 'DAY') - (1 / 86400),
-				'Last Month', TRUNC(SYSDATE, 'MONTH') - (1 / 86400),
-				'Month to Date', TRUNC(SYSDATE) - (1 / 86400),
-				'User-defined', DECODE(
-					@Prompt('Enter end date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 23:59:59'}, User:81),
-					'01/01/1800 00:00:00',
-					'',
-					@Variable('Enter end date (Leave as 01/01/1800 if using a Relative Date)')
-				),
-				'N Days Prior', SYSDATE
-			) + 1
-	)
+	AND CLINICAL_EVENT.EVENT_END_DT_TM BETWEEN
+		pi_to_gmt(
+			TO_DATE(
+				@Prompt('Enter begin date', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:0), 
+				pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+			), 
+			'CST'
+		)
+		AND pi_to_gmt(
+			TO_DATE(
+				@Prompt('Enter end date', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:1), 
+				pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+			) - 1/86400, 
+			'CST'
+		)
 
 MINUS
 
@@ -127,72 +74,18 @@ WHERE
 		)
 		AND ENCNTR_LOC_HIST.LOC_NURSE_UNIT_CD = 1101851
 	)
-	AND (
-		CLINICAL_EVENT.EVENT_END_DT_TM + 0
-			BETWEEN DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', pi_to_gmt(TRUNC(SYSDATE), pi_time_zone(2, @Variable('BOUSER'))),
-				'Yesterday', pi_to_gmt(TRUNC(SYSDATE) - 1, pi_time_zone(2, @Variable('BOUSER'))),
-				'Week to Date', pi_to_gmt(TRUNC(SYSDATE, 'DAY'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Week', pi_to_gmt(TRUNC(SYSDATE - 7, 'DAY'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Month', pi_to_gmt(TRUNC(ADD_MONTHS(SYSDATE, -1), 'MONTH'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Month to Date', pi_to_gmt(TRUNC(SYSDATE - 1, 'MONTH'), pi_time_zone(2, @Variable('BOUSER'))),
-				'User-defined', pi_to_gmt(
-					TO_DATE(
-						@Prompt('Enter begin date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:80),
-						pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
-					),
-					pi_time_zone(1, @Variable('BOUSER'))),
-				'N Days Prior', pi_to_gmt(SYSDATE - @Prompt('Days Prior to Now', 'N', , mono, free, persistent, {'0'}, User:2080), pi_time_zone(2, @Variable('BOUSER')))
-			)
-			AND DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', pi_to_gmt(TRUNC(SYSDATE) + (86399 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Yesterday', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Week to Date', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Week', pi_to_gmt(TRUNC(SYSDATE, 'DAY') - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Month', pi_to_gmt(TRUNC(SYSDATE, 'MONTH') - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Month to Date', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'User-defined', pi_to_gmt(
-					TO_DATE(
-						@Prompt('Enter end date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 23:59:59'}, User:81),
-						pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
-					),
-					pi_time_zone(1, @Variable('BOUSER'))),
-				'N Days Prior', pi_to_gmt(SYSDATE, pi_time_zone(2, @Variable('BOUSER')))
-			)
-		AND CLINICAL_EVENT.EVENT_END_DT_TM
-			BETWEEN DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', TRUNC(SYSDATE),
-				'Yesterday', TRUNC(SYSDATE) - 1,
-				'Week to Date', TRUNC(SYSDATE, 'DAY'),
-				'Last Week', TRUNC(SYSDATE - 7, 'DAY'),
-				'Last Month', TRUNC(ADD_MONTHS(SYSDATE, -1), 'MONTH'),
-				'Month to Date', TRUNC(SYSDATE - 1, 'MONTH'),
-				'User-defined', DECODE(
-					@Prompt('Enter begin date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:80),
-					'01/01/1800 00:00:00',
-					'',
-					@Variable('Enter begin date (Leave as 01/01/1800 if using a Relative Date)')
-				),
-				'N Days Prior', SYSDATE - @Prompt('Days Prior to Now', 'N', , mono, free, persistent, {0}, User:2080)
-			) - 1
-			AND DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', TRUNC(SYSDATE) + (86399 / 86400),
-				'Yesterday', TRUNC(SYSDATE) - (1 / 86400),
-				'Week to Date', TRUNC(SYSDATE) - (1 / 86400),
-				'Last Week', TRUNC(SYSDATE, 'DAY') - (1 / 86400),
-				'Last Month', TRUNC(SYSDATE, 'MONTH') - (1 / 86400),
-				'Month to Date', TRUNC(SYSDATE) - (1 / 86400),
-				'User-defined', DECODE(
-					@Prompt('Enter end date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 23:59:59'}, User:81),
-					'01/01/1800 00:00:00',
-					'',
-					@Variable('Enter end date (Leave as 01/01/1800 if using a Relative Date)')
-				),
-				'N Days Prior', SYSDATE
-			) + 1
-	)
-
+	AND CLINICAL_EVENT.EVENT_END_DT_TM BETWEEN
+		pi_to_gmt(
+			TO_DATE(
+				@Prompt('Enter begin date', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:0), 
+				pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+			), 
+			'CST'
+		)
+		AND pi_to_gmt(
+			TO_DATE(
+				@Prompt('Enter end date', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:1), 
+				pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+			) - 1/86400, 
+			'CST'
+		)
